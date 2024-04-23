f0ea2a7f2e189387c97b861caaf0b956969080ee
Jackutea <chenwansal1@163.com>
2024-04-23 17:23:29 +0800

    <shader> rename: light -> lightDir

--- assets/user/glsl330/shader_vertex_lit.fs
+++ assets/user/glsl330/shader_vertex_lit.fs
@@ -37,7 +37,7 @@ void main()
 {
     // Texel color fetching from texture sampler
     vec4 texelColor = texture(texture0, fragTexCoord);
-    vec3 lightDot = vec3(0.0);
+    vec3 lightColor = vec3(0.0);
     vec3 normal = normalize(fragNormal);
     vec3 viewD = normalize(viewPos - fragPosition);
     vec3 specular = vec3(0.0);
@@ -46,20 +46,22 @@ void main()
 
     for (int i = 0; i < lightCount; i++) {
         if (lights[i].enabled == 1) {
-            vec3 light = vec3(0.0);
+            vec3 lightDir = vec3(0.0);
 
             if (lights[i].type == LIGHT_DIRECTIONAL) {
-                light = -normalize(lights[i].target - lights[i].position);
+                lightDir = -normalize(lights[i].target - lights[i].position);
             } else if (lights[i].type == LIGHT_POINT) {
-                light = normalize(lights[i].position - fragPosition);
+                lightDir = normalize(lights[i].position - fragPosition);
             }
 
             // - Lambert (diffuse)
-            // float NdotL = dot(normal, light); // -1, 0(明暗交界线), 1
+            // float NdotL = dot(normal, lightDir); // -1, 0(明暗交界线), 1
             // - Half-Lambert (diffuse)
-            float NdotL = dot(normal, light) * 0.5 + 0.5; // 0, 0.5(明暗交界线), 1
+            float NdotL = dot(normal, lightDir) * 0.5 + 0.5; // 0, 0.5(明暗交界线), 1
             NdotL = max(NdotL, 0.0); // 防止负数
-            lightDot += lights[i].color.rgb*NdotL;
+
+            // 光照颜色
+            lightColor += lights[i].color.rgb * NdotL;
 
             float specCo = 0.0;
             // specular
@@ -67,13 +69,13 @@ void main()
                 // {
                 //     // - Phong
                 //     float shiness = 8.0; // 越大表面越光滑, 越小表面越粗糙
-                //     vec3 reflectVec = reflect(-light, normal);
+                //     vec3 reflectVec = reflect(-lightDir, normal);
                 //     specCo = pow(max(0.0, dot(viewD, reflectVec)), shiness);
                 // }
                 {
                     // - Blinn-Phong (性能更好, 效果略有差别-高光弱一些)
                     float shiness = 16.0; // 越大表面越光滑, 越小表面越粗糙
-                    vec3 halfVec = normalize(light + viewD);
+                    vec3 halfVec = normalize(lightDir + viewD);
                     float specCo = pow(max(0.0, dot(halfVec, normal)), shiness);
                 }
             }
@@ -81,10 +83,10 @@ void main()
         }
     }
 
-    finalColor = (texelColor*((colDiffuse + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
+    finalColor = (texelColor * ((colDiffuse + vec4(specular, 1.0)) * vec4(lightColor, 1.0)));
 
     // Ambient light
-    finalColor += texelColor*(ambient*0.1)*colDiffuse;
+    finalColor += texelColor * (ambient * 0.1) * colDiffuse;
 
     // Gamma correction
     finalColor = pow(finalColor, vec4(1.0/2.2));
